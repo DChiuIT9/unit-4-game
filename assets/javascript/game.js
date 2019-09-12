be32c41370bb6099d279d991d765e33f258840ef
Daniel Chiu <cycdaniel@gmail.com>
2019-09-12 18:44:19 -0500

    Added click function for the images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,11 @@
 var randomNum = Math.floor(Math.random()*101+19);
 console.log("Random Number: " + randomNum);
-
 $("#random-text").html("Random Number: " + "<b>" + randomNum + "</b>");
 
 var score = 0;
 
-$("#score-text").html("Your total score is: " + "<b>" + score + "</b>");
-
 var wins = 0;
 var losses = 0;
-
-$("#wins-text").html("Wins: " + "<b>" + wins + "</b>");
-$("#losses-text").html("Losses: " + "<b>" + losses + "</b>");
 
 var redNum = Math.floor(Math.random()*11+1);
 var blueNum = Math.floor(Math.random()*11+1);
@@ -53,3 +47,27 @@ function win_lose () {
         reset();
     }
 }
+
+$("#redpic").on("click", function(){
+    score = score + redNum;
+    $("#score-text").html("Your total score is: " + "<b>" + score + "</b>");
+    win_lose();
+});
+
+$("#bluepic").on("click", function(){
+    score = score + blueNum;
+    $("#score-text").html("Your total score is: " + "<b>" + score + "</b>");
+    win_lose();
+});
+
+$("#yellowpic").on("click", function(){
+    score = score + yellowNum;
+    $("#score-text").html("Your total score is: " + "<b>" + score + "</b>");
+    win_lose();
+});
+
+$("#greenpic").on("click", function(){
+    score = score + greenNum;
+    $("#score-text").html("Your total score is: " + "<b>" + score + "</b>");
+    win_lose();
+});
